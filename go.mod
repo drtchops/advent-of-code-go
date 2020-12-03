13f1159a76b4cb44b07cd2856a47c0d4fbcfeabc
Ian Robinson <irobinson@getresq.com>
2020-12-02 20:43:27 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/drtchops/aoc
+
+go 1.15
---
+++ solutions/day01/solver.go
@@ -0,0 +1,17 @@
+package day01
+
+type Solver struct {
+	input string
+}
+
+func New(input string) *Solver {
+	return &Solver{input: input}
+}
+
+func (d *Solver) SolveA() string {
+	return ""
+}
+
+func (d *Solver) SolveB() string {
+	return ""
+}
